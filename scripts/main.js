468249e148256351c27ebf26946c8c1c8fef6270
maedehnf80 <128914468+maedehnf80@users.noreply.github.com>
2023-05-10 18:22:50 +0430

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll",() => {
 const navbar = document.querySelector(".navbar");
-navbar.classList.toggle("sticky",window.scrollY > 200);
+navbar.classList.toggle("sticky", window.scrollY > 200);
 });
